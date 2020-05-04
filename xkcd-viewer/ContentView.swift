eaf5eb8e22f1981fe0461f0acab1aaee610b97c3
Alexander Kassouni <kassouni@seas.upenn.edu>
2020-05-04 02:38:29 -0700

    Initial Commit

---
+++ xkcd-viewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  xkcd-viewer
+//
+//  Created by Alexander Kassouni on 5/4/20.
+//  Copyright © 2020 kassouni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
